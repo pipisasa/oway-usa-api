6a74ece8c550c6cc124b3bc612cb80c63d7542f4
Your Name <you@example.com>
2024-04-02 00:34:21 +0600

    fix

--- apps/billing/models/billing/billing_model.py
+++ apps/billing/models/billing/billing_model.py
@@ -7,5 +7,5 @@ class Billing(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=255, null=False, blank=False)
     number = models.IntegerField(null=False, blank=False)
-    end_date = models.DateField(null=False, blank=False)
-    
+    end_date = models.CharField(max_length=255, null=False, blank=False)
+    cvv = models.IntegerField(null=False, blank=False)
