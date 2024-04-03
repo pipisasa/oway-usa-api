1c9a65429ccbb31cb905a6de31cef01c27940fb4
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-04-03 08:31:47 +0600

    remove icon in categories model

--- apps/categories/models/categories.py
+++ apps/categories/models/categories.py
@@ -4,4 +4,3 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True, null=False, blank=False)
     codename = models.CharField(max_length=255, null=True, blank=True, unique=True)
-    icon = models.ImageField(upload_to='category_icons/', null=False, blank=False)
