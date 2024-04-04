5fb1132f296d929cc018804ec2e13a1b404d2c11
Your Name <you@example.com>
2024-04-04 17:57:39 +0600

    added created_at to purchase model

--- apps/purchase/views/list_purchase.py
+++ apps/purchase/views/list_purchase.py
@@ -9,7 +9,7 @@ from apps.purchase import serializers
 
 
 class PurchaseList(APIView):
-    permission_classes = [IsAdminUser]
+    # permission_classes = [IsAdminUser]
 
     def get(self, request):
         paginator = PageNumberPagination()
@@ -19,8 +19,13 @@ class PurchaseList(APIView):
         result_page = paginator.paginate_queryset(purchases, request)
         serializer = serializers.PurchaseListSerializer(result_page, many=True)
 
-        for purchase in serializer.data:
-            purchase['is_paid'] = purchase['payment_confirmation'] is not None
-            purchase['request_status'] = purchase['payment_confirmation'] is not None
+        modified_data = []
 
-        return paginator.get_paginated_response(serializer.data)
+        for purchase in serializer.data:
+            modified_purchase = purchase.copy()
+            modified_purchase['is_paid'] = purchase['payment_confirmation'] is not None
+            modified_purchase['request_status'] = purchase['payment_confirmation'] is not None
+            modified_data.append(modified_purchase)
+
+        return paginator.get_paginated_response(modified_data)
+
