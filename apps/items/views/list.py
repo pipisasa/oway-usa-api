fc6d303e1587134e54f67eb0346cef402d92faba
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-06-22 01:22:27 +0600

    add filters items category

--- apps/items/views/list.py
+++ apps/items/views/list.py
@@ -8,6 +8,8 @@ from apps.shared.views.list_view import ListAPIView
 from apps.items.models import Item
 from apps.items.serializers import ItemListSerializer
 
+from .filter_helpers import text_search_filters
+
 
 class ItemListAPI(ListAPIView):
     permission_classes = [AllowAny]
@@ -18,6 +20,20 @@ class ItemListAPI(ListAPIView):
     def get_queryset(self):
         return Item.objects.all()
 
+    def _filter_queryset(self, request, queryset):
+        query_params = request.query_params.copy()
+
+        q_objects = self.build_filters(
+            query_params=query_params,
+            simple_filters=[],
+            in_filters=[],
+            boolean_filters=[],
+            range_filters=[],
+            text_search_filters=text_search_filters,
+        )
+        queryset = queryset.filter(q_objects)
+        return queryset
+
     @swagger_auto_schema(
         operation_summary="List Items",
         responses={
@@ -26,7 +42,8 @@ class ItemListAPI(ListAPIView):
     )
     def get(self, request):
         queryset = self.get_queryset()
-        base_response, _ = self.get_base_response(queryset, request)
+        filtered_queryset = self._filter_queryset(request, queryset)
+        base_response, _ = self.get_base_response(filtered_queryset, request)
 
         response_data = {
             **base_response,
