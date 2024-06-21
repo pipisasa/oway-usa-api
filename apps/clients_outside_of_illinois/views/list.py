4bfd57aafde7f10111bd9543b490eca72156c4df
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-06-22 01:26:55 +0600

    add filters

--- apps/clients_outside_of_illinois/views/list.py
+++ apps/clients_outside_of_illinois/views/list.py
@@ -5,6 +5,8 @@ from apps.clients_outside_of_illinois import serializers, model
 
 from rest_framework import permissions, status
 
+from .filter_helpers import simple_filters, text_search_filters
+
 
 class ClientsOutsideOfIllinoisListView(ListAPIView):
     permission_classes = [permissions.IsAdminUser]
@@ -15,9 +17,24 @@ class ClientsOutsideOfIllinoisListView(ListAPIView):
     def get_queryset(self):
         return model.OutsideOfIllinois.objects.all()
 
+    def _filter_queryset(self, request, queryset):
+        query_params = request.query_params.copy()
+
+        q_objects = self.build_filters(
+            query_params=query_params,
+            simple_filters=simple_filters,
+            in_filters=[],
+            boolean_filters=[],
+            range_filters=[],
+            text_search_filters=text_search_filters,
+        )
+        queryset = queryset.filter(q_objects)
+        return queryset
+
     def get(self, request):
         queryset = self.get_queryset()
-        base_response, _ = self.get_base_response(queryset, request)
+        filtered_queryset = self._filter_queryset(request, queryset)
+        base_response, _ = self.get_base_response(filtered_queryset, request)
 
         response_data = {
             **base_response,
