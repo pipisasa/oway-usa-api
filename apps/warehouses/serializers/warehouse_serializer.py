31ab0e0a83a6759a857fba3a2acd5db1dc7855d9
TumantaevBaiaman <tumantaev.baiaman.space@gmail.com>
2024-06-24 10:10:46 +0600

    fix

--- apps/warehouses/serializers/warehouse_serializer.py
+++ apps/warehouses/serializers/warehouse_serializer.py
@@ -14,7 +14,6 @@ class WarehouseCreateSerializer(serializers.ModelSerializer):
 
 
 class WarehouseListSerializer(serializers.ModelSerializer):
-    country = DetailCountrySerializer()
 
     class Meta:
         model = Warehouse
