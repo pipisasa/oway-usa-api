2c0205c8cbcc3e6f50557380b7ccb8b83b8af84a
pipisasa <emir0725mk@gmail.com>
2024-11-07 23:35:29 +0600

    fix cors

--- oway_usa_api/settings/base/corsheaders.py
+++ oway_usa_api/settings/base/corsheaders.py
@@ -5,7 +5,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://front-production-4425.up.railway.app/auth/login",
+    "https://front-production-4425.up.railway.app",
     "https://oway-usa-api-production.up.railway.app",
     "https://owaycargo.com",
 ]
